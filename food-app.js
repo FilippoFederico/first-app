da03871b5c2568e879194df846f79835eff4f5ef
Filippo Federico <filippofederico@mbp-filippo.intern>
2019-05-13 16:22:03 +0200

    first commit

---
+++ food-app.js
@@ -0,0 +1,104 @@
+
+
+
+
+    //new york = 280
+    //los angeles = 281
+    //Las Vegas = 282
+    //Washington DC = 283
+    //miami = 291
+
+function imageSection(data) {
+
+    var restaurantS = data.restaurants;
+    console.log(restaurantS)
+
+
+
+
+
+    for (i = 0; i < restaurantS.length; i++) {
+
+        var imgHome = document.querySelector('.show_img');
+        //        console.log(imgHome)
+        var restaurantData = restaurantS[i].restaurant;
+        //    console.log(restaurantData)
+        var immLink = restaurantS[i].restaurant.featured_image;
+
+        var restaurantName = restaurantData.name;
+        console.log(restaurantName)
+
+
+        var imgDiv = document.createElement('div');
+        imgDiv.setAttribute('class', 'boxes_img card text-white');
+        imgHome.appendChild(imgDiv);
+
+        var linkImg = document.createElement('img');
+        //    console.log(linkImg);
+        linkImg.setAttribute('class', 'imgs_class');
+
+        linkImg.setAttribute('src', immLink);
+        imgDiv.appendChild(linkImg);
+        var divTextImg = document.createElement('div');
+        divTextImg.setAttribute('class', 'card-img-overlay');
+        imgDiv.appendChild(divTextImg);
+        var textImg = document.createElement('p');
+        textImg.setAttribute('class', 'card-text rest_name');
+        divTextImg.appendChild(textImg);
+        textImg.innerHTML = restaurantName;
+
+
+        console.log();
+    }
+}
+
+var filterSelect = document.querySelector('.select_city');
+console.log(filterSelect)
+filterSelect.addEventListener('change', function () {
+    console.log('in onchange')
+    var options = filterSelect.querySelector('option');
+//    var count = options.length;
+//    console.log(options)
+    if (filterSelect.value == 'new_york') {
+        console.log('new york')
+        fetchData('280')
+    } else if(filterSelect.value == 'los_angeles'){
+        fetchData('281')
+    } else if(filterSelect.value == 'miami'){
+        fetchData('291')
+    } else if(filterSelect.value == 'las_vegas'){
+        fetchData('282')
+    } else if(filterSelect.value == 'washington'){
+        fetchData('283')
+    }
+})
+
+function fetchData(id) {
+    const url = 'https://developers.zomato.com/api/v2.1/search?entity_id=' + id + '&entity_type=city&count=50';
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+
+    fetch(proxyUrl + url, {
+
+            headers: {
+                'user-key': '8b84ccd1a11a50818a61688023d29f49',
+
+            }
+        }) // Call the fetch function passing the url of the API as a parameter
+        .then(function (response) {
+            console.log(response)
+            return response.json();
+            // Your code for handling the data you get from the API
+            showImages(data)
+
+
+        })
+        .then(function (data) {
+            console.log(data.restaurants[0]);
+
+            imageSection(data)
+
+
+        })
+        .catch(err => console.log(err));
+
+}
